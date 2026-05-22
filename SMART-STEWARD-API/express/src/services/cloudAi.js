84cd88bc51fa59d811ea6dca8d709844d127fef1
kkreiju <arjayninosaguisa@gmail.com>
2026-05-23 01:18:37 +0800

    chore: Removed usage value in response

--- SMART-STEWARD-API/express/src/services/cloudAi.js
+++ SMART-STEWARD-API/express/src/services/cloudAi.js
@@ -167,10 +167,7 @@ const generateCloudResponse = async (mediaFile) => {
     });
 
     const parsed = JSON.parse(response.choices[0].message.content);
-    return {
-      response: enrichReportableFlag(parsed),
-      usage: response.usage,
-    };
+    return enrichReportableFlag(parsed);
   } finally {
     // Clean up extracted video frames
     for (const framePath of tempFramePaths) {
